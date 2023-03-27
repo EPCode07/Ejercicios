f10f173380746212eaa4d630cd3be5a2308b083d
EPCode07 <esuyonpanta@gmail.com>
2023-03-26 19:03:38 -0500

    modified:   Ejercicio17.py

--- Ejercicio17.py
+++ Ejercicio17.py
@@ -1,24 +1,21 @@
-print('Ejercicio N° 17 : Suma de Enteros Positivos Hasta "n" números\n')
-numero_n = int(input('Número positivo hasta el que desea sumar: '))
-
-try:
-    if numero_n > 0:
-except:
-    numero_n = int(input('Número positivo hasta el que desea sumar: '))
-    for i in range(numero_n):
-            total_suma = (numero_n*(numero_n + 1 ))/2
-    print(total_suma)
+print('Ejercicio N° 17 : Suma de enteros positivos hasta "n" números\n')
 
 def suma(x):
     for i in range(x):
         total_suma = (x*(x+1))/2
-    print()
-    
-try:    
-    if numero_n > 0:
-     suma(numero_n)
-except:
-    numero_n = int(input('Número positivo hasta el que desea sumar: '))
-    suma(numero_n)
+    print(total_suma)
+
+while True:
+    try:
+        numero_n = int(input('Número positivo hasta el que desea sumar: '))     
+    except ValueError:
+        print("Debes escribir un número.")
+        continue
+    if numero_n < 0:
+        print("Debes escribir un número positivo.")
+        continue
+    else:
+        break
+suma(numero_n)
 
 
