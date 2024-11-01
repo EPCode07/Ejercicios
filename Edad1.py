f7ab1b4bbb2bb0059e7bcfa6aeed0385ac70a7f6
Erick Dilvert Suyon Panta <125427364+EPCode07@users.noreply.github.com>
2024-11-01 09:34:40 -0500

    Cambio1

--- Edad1.py
+++ Edad1.py
@@ -2,7 +2,7 @@ print('**Verificación de edad**\n')
 e = int(input('Hola, ingresa tu edad: '))
 
 if e>=18:
-    print('\nEres mayor de edad')
+    print('\nEres de edad')
 else:
     print('\nEres menor de edad')
 print('Fin.')
